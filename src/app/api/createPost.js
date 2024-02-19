9095a0e06047aa013a3f2699f323cbebfaaff329
clover00000011 <fclover996@gmail.com>
2024-02-19 12:15:54 -0800

    first commit

--- src/app/api/createPost.js
+++ src/app/api/createPost.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const { title, content } = req.body;
 
   const client = new Client({
-    connectionString: "postgres://default:6p8KNCuBHzAU@ep-aged-moon-a4o26jqp-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    connectionString: process.env.POSTGRES_URL,
     ssl: {
       rejectUnauthorized: false,
     },
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,7 @@
-// pages/dashboard/create/page.tsx
-
-import CreatePostForm from '../ui/postForm';
-
-const Dashboard = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Create New Blog Post</h1>
-      <CreatePostForm />
-    </main>
-  );
-};
-
-export default Dashboard;
+export default function Dashboard() {
+    return (
+      <main>
+        
+      </main>
+    );
+}
