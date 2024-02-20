4941a7e7a0dcb9f8136887da52b45adb08115ff9
clover00000011 <fclover996@gmail.com>
2024-02-19 16:34:35 -0800

    first commit

--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Create New Blog Post</h1>
-      <CreatePostForm />
+      <CreatePostForm/>
     </main>
   );
 };
